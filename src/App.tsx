69b122d58460735521f0fce95fa67fd7c5bb656c
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-04 14:42:48 +0100

    3)Display Data: refacto isLoading func

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ interface Post {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [page, setPage] = useState(0)
   
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,23 +19,20 @@ function App() {
       setIsLoading(true);
 
       try{
-        const response = await fetch(`${BASE_URL}/posts`);
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`);
         const posts = await response.json() as Post[];
         setPosts(posts);
       }catch (e: any){
         setError(e);
+      } finally{
+        setIsLoading(false);
       }
 
 
-      setIsLoading(false);
     };
 
     fetchPosts();
-  }, []);
-
-  if(isLoading){
-    return <div>Loading...</div>
-  }
+  }, [page]);
 
   if(error){
     return <div>Something went wrong ! Please try again</div>
@@ -44,11 +42,16 @@ function App() {
 
     <div className='tutorial'>
       <h1 className='mb-4 text-2xl'> Data Fetching in React</h1>
-      <ul>
-        {posts.map((post) => {
-          return <li key={post.id}>{post.title}</li>
-        })}
-      </ul>
+      <button onClick={() => setPage(page+1)}> Increase Page ({page})</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading &&      
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>
+          })}
+        </ul>
+      }
+      
     </div>
    
   )
